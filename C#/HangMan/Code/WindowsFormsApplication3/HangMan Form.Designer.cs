3dd77b76f0ad73ca19a08f58cd5ac4b75bf04229
Alexandru Știrbu <34741250+Akrielz@users.noreply.github.com>
2019-09-17 20:25:20 +0300

    Add files via upload

---
+++ C#/HangMan/Code/WindowsFormsApplication3/HangMan Form.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class HangMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HangMan));
+            this.insertLetterBox = new System.Windows.Forms.RichTextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.drawingArea = new System.Windows.Forms.PictureBox();
+            this.displayWordLabel = new System.Windows.Forms.Label();
+            this.usedLettersLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // insertLetterBox
+            // 
+            this.insertLetterBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F);
+            this.insertLetterBox.Location = new System.Drawing.Point(260, 599);
+            this.insertLetterBox.Name = "insertLetterBox";
+            this.insertLetterBox.Size = new System.Drawing.Size(70, 70);
+            this.insertLetterBox.TabIndex = 2;
+            this.insertLetterBox.Text = "";
+            // 
+            // guessButton
+            // 
+            this.guessButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.guessButton.Location = new System.Drawing.Point(12, 599);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(200, 70);
+            this.guessButton.TabIndex = 3;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // restartButton
+            // 
+            this.restartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.restartButton.Location = new System.Drawing.Point(1052, 599);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(200, 70);
+            this.restartButton.TabIndex = 4;
+            this.restartButton.Text = "Try Again";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // drawingArea
+            // 
+            this.drawingArea.Location = new System.Drawing.Point(0, 0);
+            this.drawingArea.Name = "drawingArea";
+            this.drawingArea.Size = new System.Drawing.Size(506, 432);
+            this.drawingArea.TabIndex = 5;
+            this.drawingArea.TabStop = false;
+            // 
+            // displayWordLabel
+            // 
+            this.displayWordLabel.AutoSize = true;
+            this.displayWordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.displayWordLabel.Location = new System.Drawing.Point(30, 435);
+            this.displayWordLabel.Name = "displayWordLabel";
+            this.displayWordLabel.Size = new System.Drawing.Size(111, 44);
+            this.displayWordLabel.TabIndex = 6;
+            this.displayWordLabel.Text = "Word";
+            // 
+            // usedLettersLabel
+            // 
+            this.usedLettersLabel.AutoSize = true;
+            this.usedLettersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.usedLettersLabel.Location = new System.Drawing.Point(830, 30);
+            this.usedLettersLabel.Name = "usedLettersLabel";
+            this.usedLettersLabel.Size = new System.Drawing.Size(177, 31);
+            this.usedLettersLabel.TabIndex = 7;
+            this.usedLettersLabel.Text = "Used Letters:";
+            // 
+            // HangMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.usedLettersLabel);
+            this.Controls.Add(this.displayWordLabel);
+            this.Controls.Add(this.drawingArea);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.insertLetterBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "HangMan";
+            this.Text = "HangMan";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox insertLetterBox;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.PictureBox drawingArea;
+        private System.Windows.Forms.Label displayWordLabel;
+        private System.Windows.Forms.Label usedLettersLabel;
+    }
+}
+
---
+++ C#/HangMan/Code/WindowsFormsApplication3/HangMan Form.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication3
+{
+    public partial class HangMan : Form
+    {
+        Random randomSeed = new Random();
+
+        Point hangPoint, firstCorner, secondCorner;
+
+        Graphics drawArea;
+
+        List <string> possibleWords = new List<string>();
+
+        List<char> usedLetters;
+
+        List<char> displayedWord;
+
+        string chosenWord;
+
+        int lives, requiredLetters;
+
+        public void erase(Point first, Point second)
+        {
+            Rectangle filledRectangle = new Rectangle(first.X, first.Y, second.X, second.Y);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            drawArea.FillRectangle(whiteBrush, filledRectangle);
+        }
+
+        public void drawHang(PaintEventArgs graphicPanel, int wide = 10)
+        {
+            Pen blackPen = new Pen(Color.Black, wide);
+
+            Point first = new Point(50, 400);
+            Point second = new Point(first.X+100, first.Y);
+
+            Point third = new Point(first.X, first.Y - 40);
+            Point fourth = new Point(second.X, third.Y);
+
+            Point middle = new Point((third.X + fourth.X) / 2, fourth.Y);
+
+            Point top1 = new Point(middle.X, middle.Y - 300);
+            Point top2 = new Point(top1.X + 100, top1.Y);
+
+            Point final = new Point(top2.X, top2.Y + 40);
+
+            hangPoint = final;
+
+            drawArea.DrawLine(blackPen, first, third);
+            drawArea.DrawLine(blackPen, second, fourth);
+            drawArea.DrawLine(blackPen, third, fourth);
+            drawArea.DrawLine(blackPen, middle, top1);
+            drawArea.DrawLine(blackPen, top1, top2);
+            drawArea.DrawLine(blackPen, top2, final);
+        }
+
+        public void drawHuman(int wide = 10)
+        {
+            Pen blackPen = new Pen(Color.Black, wide);
+
+            Point head = new Point(hangPoint.X-75/2, hangPoint.Y);
+
+            Point neck = new Point(head.X+(75/2), head.Y+75);
+            Point leftArm = new Point(neck.X - 50, neck.Y + 50);
+            Point rightArm = new Point(neck.X + 50, leftArm.Y);
+
+            Point bottom = new Point(neck.X, neck.Y + 100);
+            Point leftLeg = new Point(bottom.X - 50, bottom.Y + 50);
+            Point rightLeg = new Point(bottom.X + 50, leftLeg.Y);
+
+            firstCorner.X = leftLeg.X-5;
+            firstCorner.Y = head.Y-5;
+
+            secondCorner.X = rightLeg.X+5;
+            secondCorner.Y = rightLeg.Y+5;
+
+            switch (lives)
+            {
+                case 5:
+                    drawArea.DrawEllipse(blackPen, head.X, head.Y, 75, 75);
+                    break;
+                case 4:
+                    drawArea.DrawLine(blackPen, neck, bottom);
+                    break;
+                case 3:
+                    drawArea.DrawLine(blackPen, neck, leftArm);
+                    break;
+                case 2:
+                    drawArea.DrawLine(blackPen, neck, rightArm);
+                    break;
+                case 1:
+                    drawArea.DrawLine(blackPen, bottom, leftLeg);
+                    break;
+                case 0:
+                    drawArea.DrawLine(blackPen, bottom, rightLeg);
+                    break;
+            }
+
+        }
+
+        public void printWord()
+        {
+            char[] buffer = new char[2*chosenWord.Length];
+            for (int index = 0; index < chosenWord.Length; index++)
+            {
+                buffer[2 * index] = displayedWord[index];
+                buffer[2 * index + 1] = ' ';
+            }
+            string result = new string(buffer);
+            displayWordLabel.Text = result;
+        }
+
+        public void printUsedLetters()
+        {
+            char[] buffer = new char[2*usedLetters.Count];
+            for (int index = 0; index < usedLetters.Count; index++)
+            {
+                buffer[2 * index] = usedLetters[index];
+                if ( index != usedLetters.Count-1 )
+                    buffer[2 * index + 1] = ',';
+            }
+            string result = new string(buffer);
+            usedLettersLabel.Text = "Used letters:\n" + result;
+        }
+
+        public void readWordsFromFile()
+        {
+            StreamReader stream = new StreamReader("words.in");
+
+            string data;
+            while (true)
+            {
+                data = stream.ReadLine();
+                if (data == null)
+                    break;
+                possibleWords.Add(data);
+            }
+        }
+
+        public void chooseWord()
+        {
+            int position = randomSeed.Next(possibleWords.Count);
+            chosenWord = possibleWords[position];
+            chosenWord = chosenWord.ToUpper();
+            displayedWord = new List<char>();
+            for (int index = 0; index < chosenWord.Length; index++)
+                displayedWord.Add('_');
+        }
+
+        public bool verifyLetter(char letter)
+        {
+            usedLetters.Add(letter);
+            printUsedLetters();
+            bool contained = false;
+            for ( int index = 0; index < chosenWord.Length; index++)
+            {
+                if ( letter == chosenWord[index])
+                {
+                    contained = true;
+                    displayedWord[index] = letter;
+                    requiredLetters--;
+                }
+            }
+            return contained;
+        }
+
+        public bool isInList(char letter)
+        {
+            foreach(char character in usedLetters)
+            {
+                if (character == letter)
+                    return true;
+            }
+            return false;
+        }
+
+        public void resetVariables()
+        {
+            lives = 6;
+            usedLetters = new List<char>();
+            chooseWord();
+            requiredLetters = chosenWord.Length;
+            verifyLetter(chosenWord[0]);
+            if ( isInList(chosenWord[chosenWord.Length - 1]) == false )
+                verifyLetter(chosenWord[chosenWord.Length - 1]);
+            printWord();
+            erase(firstCorner, secondCorner);
+            
+        }
+
+        public HangMan()
+        {
+            InitializeComponent();
+            readWordsFromFile();
+            drawArea = drawingArea.CreateGraphics();
+            resetVariables();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs graphicPanel)
+        {
+            drawHang(graphicPanel);
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            char givenLetter;
+            string givenWord;
+
+            givenWord = insertLetterBox.Text;
+            if (givenWord.Length > 1)
+            {
+                MessageBox.Show("Please enter just a letter");
+                insertLetterBox.Text = "";
+                return;
+            }
+
+            if (givenWord.Length == 0)
+            {
+                MessageBox.Show("You must enter at least one letter before trying to guess");
+                return;
+            }
+
+            givenWord = givenWord.ToUpper();
+            givenLetter = givenWord[0];
+
+            if ( !(givenLetter >= 'A' && givenLetter <= 'Z') ){
+                MessageBox.Show("Your input must contain a letter");
+                insertLetterBox.Text = "";
+                return;
+            }
+
+            if ( isInList(givenLetter) )
+            {
+                MessageBox.Show("You have already used this letter");
+                insertLetterBox.Text = "";
+                return;
+            }
+
+            if (verifyLetter(givenLetter))
+                printWord();
+            else
+            {
+                lives--;
+                drawHuman();
+            }
+
+            if (lives == 0)
+            {
+                MessageBox.Show("You lost");
+                resetVariables();
+            }
+
+            if ( requiredLetters == 0 )
+            {
+                MessageBox.Show("You won");
+                resetVariables();
+            }
+
+            insertLetterBox.Text = "";
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            resetVariables();
+            insertLetterBox.Text = "";
+        }
+    }
+}
